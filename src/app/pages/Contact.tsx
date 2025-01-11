b79b3297246e4bb0e532796235b5ad2ad6c208b4
Luca <luca.lelaurain@gmail.com>
2025-01-11 08:46:46 +0100

    env.local

--- src/app/pages/Contact.tsx
+++ src/app/pages/Contact.tsx
@@ -39,6 +39,10 @@ const Contact: React.FC = () => {
         formData,
         process.env.NEXT_PUBLIC_EMAILJS_USER_ID || ""
       );
+      console.log("Service ID:", process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID);
+console.log("Template ID:", process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID);
+console.log("User ID:", process.env.NEXT_PUBLIC_EMAILJS_USER_ID);
+
 
       setFormStatus("Votre message a été envoyé avec succès !");
       setFormData({ name: "", email: "", message: "" });
